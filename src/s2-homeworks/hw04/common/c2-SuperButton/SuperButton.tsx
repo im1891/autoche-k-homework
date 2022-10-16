78121a074058f8b3123ef334aedbf1d68281e49d
Igor Muratov <im1891@gmail.com>
2022-10-16 13:51:33 +0300

    Refactored HW4.

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,12 +17,14 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-         + (disabled
-                 ? s.disabled
-             : xType === 'red'
-                    ? s.red : ' ')
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+
+    const finalClassName = `${s.button} ${disabled ? s.disabled : xType === 'red' ? s.red : ''} ${className ? className : ''}`
+    /*s.button
+        // + (disabled
+        //         ? ...
+        //         : xType === 'red'
+        //             ? ...
+        + (className ? ' ' + className : '') // задачка на смешивание классов*/
 
     return (
         <button
