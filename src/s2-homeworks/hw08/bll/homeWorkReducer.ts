e54fe4db9928f87dda9f00c864a4871397b73865
Igor Muratov <im1891@gmail.com>
2022-10-30 20:03:50 +0200

    Completed HW8.

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let stateCopy= [...state];
 
-            return state // need to fix
+            if (action.payload === 'up') {
+                stateCopy =  stateCopy.sort( (e1,e2) => e1.name > e2.name ? 1 : -1 )
+            } else if(action.payload === 'down' ) {
+                stateCopy =  stateCopy.sort( (e1,e2) => e1.name < e2.name ? 1 : -1 )
+            }
+            return stateCopy  // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return [...state].filter( el => el.age > 18) // need to fix
         }
         default:
             return state
