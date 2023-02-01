8605212a576626fbf4756f53c584dcb29d791a0c
Igor Muratov <im1891@gmail.com>
2023-02-01 15:11:19 +0200

    Refactored HW9.

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -25,6 +25,12 @@ function Clock() {
         second: "2-digit",
     })
 
+    let fullDate = Intl.DateTimeFormat('ru', {
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit"
+    })
+
     const start = () => {
         debugger
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
@@ -50,7 +56,7 @@ function Clock() {
     }
 
     const stringTime = time.format(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date?.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = fullDate.format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
 
